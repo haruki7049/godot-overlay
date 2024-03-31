e0f46684d8a7528d6b78215ec44389e50277f54b
haruki7049 <tontonkirikiri@gmail.com>
2024-03-31 11:26:12 +0900

    chore: fuga

--- default.nix
+++ default.nix
@@ -48,7 +48,7 @@ self: super:
       version = "4.2.1";
 
       src = super.fetchzip {
-        url = "https://downloads.tuxfamily.org/godotengine/${version}/mono/Godot_v${version}-stable_mono_x11_64.zip";
+        url = "https://downloads.tuxfamily.org/godotengine/${version}mono/Godot_${version}-stable_mono_linux_x86_64.zip";
         hash = "sha256-2aNeiR/bktqr2LDN9TWr67LDB0ai9S0XKfexmTb0TQg=";
       };
 
@@ -73,7 +73,7 @@ self: super:
       installPhase = ''
         mkdir -p $out/bin $out/opt/godot-mono
 
-        install -m 0755 Godot_v${version}-stable_mono_x11.64 $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64
+        install -m 0755 Godot_v${version}-stable_mono_linux.x86_64 $out/opt/godot-mono/Godot_v${version}-stable_mono_linux.x86_64
         cp -r GodotSharp $out/opt/godot-mono
 
         ln -s $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64 $out/bin/${pname}-${version}
