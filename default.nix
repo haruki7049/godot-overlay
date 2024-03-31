dd97b37f1af97d7624344db48d4187dfb31d1a24
haruki7049 <tontonkirikiri@gmail.com>
2024-03-31 10:59:42 +0900

    chore: hoge

--- default.nix
+++ default.nix
@@ -1,7 +1,88 @@
-{ pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem, version }:
-let inherit (pkgs) callPackage;
-in rec {
-  godot = callPackage ./overlays/godot { inherit version; };
-  #godotHeadless = callPackage ./overlays/godotHeadless { godotBin = godot; };
-  #godotMono = callPackage ./overlays/godotMono { godotBin = godot; };
+self: super:
+{
+  godot-mono-bin = {
+    "3.5.1" = super.stdenv.mkDerivation rec {
+      pname = "godot-mono-bin";
+      version = "3.5.1";
+
+      src = super.fetchzip {
+        url = "https://downloads.tuxfamily.org/godotengine/${version}/mono/Godot_v${version}-stable_mono_x11_64.zip";
+        hash = "sha256-2aNeiR/bktqr2LDN9TWr67LDB0ai9S0XKfexmTb0TQg=";
+      };
+
+      nativeBuildInputs = [ super.autoPatchelfHook super.makeWrapper super.unzip ];
+
+      buildInputs = [
+        super.udev
+        super.alsaLib
+        super.xorg.libXcursor
+        super.xorg.libXinerama
+        super.xorg.libXrandr
+        super.xorg.libXrender
+        super.xorg.libX11
+        super.xorg.libXi
+        super.libpulseaudio
+        super.libGL
+        super.zlib
+      ];
+
+      libraries = super.lib.makeLibraryPath buildInputs;
+
+      installPhase = ''
+        mkdir -p $out/bin $out/opt/godot-mono
+
+        install -m 0755 Godot_v${version}-stable_mono_x11.64 $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64
+        cp -r GodotSharp $out/opt/godot-mono
+
+        ln -s $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64 $out/bin/${pname}-${version}
+      '';
+
+      postFixup = ''
+        wrapProgram $out/bin/${pname}-${version} \
+          --set LD_LIBRARY_PATH ${libraries}
+      '';
+    };
+
+    "4.2.1" = super.stdenv.mkDerivation rec {
+      pname = "godot-mono-bin";
+      version = "4.2.1";
+
+      src = super.fetchzip {
+        url = "https://downloads.tuxfamily.org/godotengine/${version}/mono/Godot_v${version}-stable_mono_x11_64.zip";
+        hash = "sha256-2aNeiR/bktqr2LDN9TWr67LDB0ai9S0XKfexmTb0TQg=";
+      };
+
+      nativeBuildInputs = [ super.autoPatchelfHook super.makeWrapper super.unzip ];
+
+      buildInputs = [
+        super.udev
+        super.alsaLib
+        super.xorg.libXcursor
+        super.xorg.libXinerama
+        super.xorg.libXrandr
+        super.xorg.libXrender
+        super.xorg.libX11
+        super.xorg.libXi
+        super.libpulseaudio
+        super.libGL
+        super.zlib
+      ];
+
+      libraries = super.lib.makeLibraryPath buildInputs;
+
+      installPhase = ''
+        mkdir -p $out/bin $out/opt/godot-mono
+
+        install -m 0755 Godot_v${version}-stable_mono_x11.64 $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64
+        cp -r GodotSharp $out/opt/godot-mono
+
+        ln -s $out/opt/godot-mono/Godot_v${version}-stable_mono_x11.64 $out/bin/${pname}-${version}
+      '';
+
+      postFixup = ''
+        wrapProgram $out/bin/${pname}-${version} \
+          --set LD_LIBRARY_PATH ${libraries}
+      '';
+    };
+  };
 }
