52ab866bf49062aab891e3948e67a330899747b1
haruki7049 <tontonkirikiri@gmail.com>
2024-07-17 10:21:13 +0900

    style: nix fmt

--- default.nix
+++ default.nix
@@ -50,48 +50,48 @@ in
                 --set LD_LIBRARY_PATH ${libraries}
             '';
           };
-          mkBinaryInstallV4 = { pname ? "godot-mono-bin", version, url, sha256 }:
-            stdenv.mkDerivation rec {
-              inherit pname version;
+        mkBinaryInstallV4 = { pname ? "godot-mono-bin", version, url, sha256 }:
+          stdenv.mkDerivation rec {
+            inherit pname version;
 
-              src = fetchzip {
-                inherit url sha256;
-              };
-
-              nativeBuildInputs = [
-                super.autoPatchelfHook
-                super.makeWrapper
-                super.unzip
-              ];
-
-              buildInputs = [
-                super.udev
-                super.alsaLib
-                super.xorg.libXcursor
-                super.xorg.libXinerama
-                super.xorg.libXrandr
-                super.xorg.libXrender
-                super.xorg.libX11
-                super.xorg.libXi
-                super.libpulseaudio
-                super.libGL
-                super.zlib
-              ];
-
-              libraries = lib.makeLibraryPath buildInputs;
-
-              installPhase = ''
-                mkdir -p $out/bin $out/opt/godot-mono
-                install -m 0755 Godot_v${version}-stable_mono_linux.x86_64 $out/opt/godot-mono/Godot_v${version}-stable_mono_linux.x86_64
-                cp -r GodotSharp $out/opt/godot-mono
-                ln -s $out/opt/godot-mono/Godot_v${version}-stable_mono_linux.x86_64 $out/bin/${pname}-${version}
-              '';
-
-              postFixup = ''
-                wrapProgram $out/bin/${pname}-${version} \
-                  --set LD_LIBRARY_PATH ${libraries}
-              '';
+            src = fetchzip {
+              inherit url sha256;
             };
+
+            nativeBuildInputs = [
+              super.autoPatchelfHook
+              super.makeWrapper
+              super.unzip
+            ];
+
+            buildInputs = [
+              super.udev
+              super.alsaLib
+              super.xorg.libXcursor
+              super.xorg.libXinerama
+              super.xorg.libXrandr
+              super.xorg.libXrender
+              super.xorg.libX11
+              super.xorg.libXi
+              super.libpulseaudio
+              super.libGL
+              super.zlib
+            ];
+
+            libraries = lib.makeLibraryPath buildInputs;
+
+            installPhase = ''
+              mkdir -p $out/bin $out/opt/godot-mono
+              install -m 0755 Godot_v${version}-stable_mono_linux.x86_64 $out/opt/godot-mono/Godot_v${version}-stable_mono_linux.x86_64
+              cp -r GodotSharp $out/opt/godot-mono
+              ln -s $out/opt/godot-mono/Godot_v${version}-stable_mono_linux.x86_64 $out/bin/${pname}-${version}
+            '';
+
+            postFixup = ''
+              wrapProgram $out/bin/${pname}-${version} \
+                --set LD_LIBRARY_PATH ${libraries}
+            '';
+          };
       in
       {
         "3.5.1" = mkBinaryInstallV3 {
