faae09768cb4ec86b0fde95f94ca55ceb76959c3
haruki7049 <tontonkirikiri@gmail.com>
2024-07-17 10:01:49 +0900

    feat(example-dir): Init v3.5.3-mono.nix

---
+++ example/v3.5.3-mono.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { inherit overlays; }
+, overlays ? [ (import ../default.nix) ]
+, godotVersion ? "3.5.3"
+, mkShell ? pkgs.mkShell
+}:
+
+mkShell {
+  packages = [
+    pkgs.godot-bin.mono.${godotVersion}
+    pkgs.dotnet-sdk
+  ];
+}
